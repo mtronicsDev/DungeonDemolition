4eb9e8521ca0e5ff0b4dcf141a4634f97fab1a16
lukDev <luk4437@googlemail.com>
2014-05-19 21:28:54 +0200

    player is now fully moveable

--- src/DungeonDemolition/Player.java
+++ src/DungeonDemolition/Player.java
@@ -5,30 +5,28 @@ import java.awt.event.KeyEvent;
 
 public class Player {
 
-    public Point position;
+    public float[] position;
     public Point rotation;
 
     public Player(Point position, Point rotation) {
 
-        this.position = position;
+        this.position = new float[] {position.x, position.y};
         this.rotation = rotation;
 
     }
 
     public void update() {
 
-        if (Input.isKeyPressed(KeyEvent.VK_W)) position.y -= 50 * TimeHelper.deltaTime;
-        if (Input.isKeyPressed(KeyEvent.VK_S)) position.y += 50 * TimeHelper.deltaTime;
-        if (Input.isKeyPressed(KeyEvent.VK_A)) position.x -= 50 * TimeHelper.deltaTime;
-        if (Input.isKeyPressed(KeyEvent.VK_D)) position.x += 50 * TimeHelper.deltaTime;
+        if (Input.isKeyPressed(KeyEvent.VK_W)) position[1] -= 50 * TimeHelper.deltaTime;
+        if (Input.isKeyPressed(KeyEvent.VK_S)) position[1] += 50 * TimeHelper.deltaTime;
+        if (Input.isKeyPressed(KeyEvent.VK_A)) position[0] -= 50 * TimeHelper.deltaTime;
+        if (Input.isKeyPressed(KeyEvent.VK_D)) position[0] += 50 * TimeHelper.deltaTime;
 
-        if (position.x < 0 ) position.x = 0;
-        else if (position.x > ObjectController.display.size.x) position.x = ObjectController.display.size.x;
+        if (position[0] < 0 ) position[0] = 0;
+        else if (position[0] > ObjectController.display.size.x) position[0] = ObjectController.display.size.x;
 
-        if (position.y < 0 ) position.y = 0;
-        else if (position.y > ObjectController.display.size.y) position.y = ObjectController.display.size.y;
-
-        System.out.println(position);
+        if (position[1] < 0 ) position[1] = 0;
+        else if (position[1] > ObjectController.display.size.y) position[1] = ObjectController.display.size.y;
 
     }
 
