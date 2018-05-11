1f6592bf32db31bb2a1b42631472dc04bdf1b07a
mtronicsDev <maxi.schmeller@live.de>
2018-05-11 14:19:31 +0200

    oaschloch

--- src/DungeonDemolition/objects/dungeons/TileInteractionMethod.java
+++
@@ -1,9 +0,0 @@
-package dungeonDemolition.objects.dungeons;
-
-import dungeonDemolition.objects.entities.Player;
-
-public interface TileInteractionMethod {
-
-    public void interact(Player player);
-
-}
